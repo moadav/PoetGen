d3be9769ee8ce42f984d267d7ab948b8a550e67c
Mohammed Ali Davami <mohammed.ali.davami@no.experis.com>
2022-11-17 15:02:28 +0100

    added poetry page and display correct poetry lines

--- PoetGen/src/app/components/form/form.component.ts
+++ PoetGen/src/app/components/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthorPoetry } from 'src/app/models/authorPoetry';
 import { Authors } from 'src/app/models/authors.model';
 import { AuthorTitle } from 'src/app/models/authorTitle.model';
@@ -9,15 +10,20 @@ import { PoetryApiService } from 'src/app/services/poetry-api.service';
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent{
+
+export class FormComponent implements OnInit{
 public error:any;
 public loading:boolean = false;
 public authorsList:any
 public selectedValue:any
 public authorTitles:any
+public authorPoetry:any
+ngOnInit(): void {
+  this.getAllAuthors();
+      
+}
 
-constructor(private readonly poetryApiService: PoetryApiService) { 
-    this.getAllAuthors();
+constructor(private readonly poetryApiService: PoetryApiService, private route: Router) { 
   }
 
   public getAllAuthors():void{
@@ -54,7 +60,10 @@ constructor(private readonly poetryApiService: PoetryApiService) {
     this.poetryApiService.getAuthorTitlePoetry(this.selectedValue, value.title).subscribe({
       next: (response: AuthorPoetry[] | undefined) => {
         
-        this.authorTitles = response;        
+        this.authorPoetry = response;
+        this.route.navigate(['/Poetry'], { state: { data: this.authorPoetry} });
+
+                
       },
       error: ( response:any) => {
         this.error = response;
--- PoetGen/src/app/pages/poetry/poetry.component.ts
+++ PoetGen/src/app/pages/poetry/poetry.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { AuthorPoetry } from 'src/app/models/authorPoetry';
 
 @Component({
   selector: 'app-poetry',
   templateUrl: './poetry.component.html',
   styleUrls: ['./poetry.component.css']
 })
-export class PoetryComponent implements OnInit {
+export class PoetryComponent{
+public text:AuthorPoetry = {title: "", author: "s", linecount: "", lines: []}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private readonly routers: Router,
+  private activatedroute: ActivatedRoute) { 
+    const state = this.routers.getCurrentNavigation()!.extras.state;
+    this.ConvertRouteToText(state);
   }
+
+private ConvertRouteToText(obj:any):void{ 
+  console.log(obj.data[0]);
+  
+  if(obj.data[0]) 
+    this.text = obj.data[0];
+}
+
+ 
 
 }
