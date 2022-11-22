94cb91dee3c4d924ed7dbfb5144c68f909544a1b
Mohammed Ali Davami <mohammed.ali.davami@no.experis.com>
2022-11-22 12:31:06 +0100

    added more styling and fixed poem format

--- PoetGen/src/app/pages/poetry/poetry.component.ts
+++ PoetGen/src/app/pages/poetry/poetry.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
 import { ActivatedRoute, Route, Router } from '@angular/router';
 import { AuthorPoetry } from 'src/app/models/authorPoetry';
 
@@ -9,18 +10,27 @@ import { AuthorPoetry } from 'src/app/models/authorPoetry';
 })
 export class PoetryComponent{
 public text:AuthorPoetry = {title: "", author: "s", linecount: "", lines: []}
+public title:string = "";
 
   constructor(private readonly routers: Router,
-  private activatedroute: ActivatedRoute) { 
+  private activatedroute: ActivatedRoute, private readonly route: Router) { 
     const state = this.routers.getCurrentNavigation()!.extras.state;
+    if(!state)
+      this.route.navigate(['/Home']);
+
     this.ConvertRouteToText(state);
+   
   }
 
 private ConvertRouteToText(obj:any):void{ 
   console.log(obj.data[0]);
   
-  if(obj.data[0]) 
+  if(obj.data[0]) {
     this.text = obj.data[0];
+    this.title = this.text.title;
+    
+  }
+  
 }
 
  
